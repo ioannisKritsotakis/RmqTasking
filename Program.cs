e898a7d2746f06927443103fc944ca69e437f896
gianniskr <giannis21kr@gmail.com>
2021-01-30 23:30:23 +0200

    add

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RmqTasking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rec = new Receiver();
+            rec.Start();
+        }
+    }
+}
